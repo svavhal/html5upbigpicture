2022f7bb982c6dee1afa738b2bb07fc943bf8ec6
pravin vaja <pravin.vaja@kasturi.com>
2014-03-08 16:41:58 +0530

    undefined sessionscontroller in routes.rb

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,12 +3,13 @@ class SessionController < ApplicationController
     # @user = User.find_or_create_from_auth_hash(auth_hash)
     # self.current_user = @user
     # redirect_to '/'
+    render nothing: true
   end
   protected
 
   def auth_hash
     request.env['omniauth.auth']
-    print "#{request.env['omniauth.auth']}"
+    print "------------------#{request.env['omniauth.auth'].to_json}----------------------"
   end
 
 end
